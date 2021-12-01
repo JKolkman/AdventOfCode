550d434cb41d9e0b1f5f55628106e9a319531957
Joost Kolkman <Joost.Kolkman@topicus.nl>
2021-12-01 15:59:07 +0100

    Initial commit

---
+++ AdventCalendarCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventCalendarCode.day1;
+
+namespace AdventCalendarCode
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            new Day1().Run();
+        }
+    }
+}
---
+++ AdventCalendarCode/day1/Day1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventCalendarCode.day1
+{
+    internal class Day1
+    {
+        private int[] _numbers;
+        public void Run()
+        {
+            var lines = System.IO.File.ReadAllLines(@"C:\Users\Joost Kolkman\RiderProjects\AdventCalendarCode\AdventCalendarCode\day1\Day1-Input");
+            _numbers = Array.ConvertAll(lines, int.Parse);
+            
+            Console.WriteLine("Results of Day 1");
+            Console.WriteLine(Task1());
+            Console.WriteLine(Task2());
+        }
+        private string Task1()
+        {
+            var counter = 0;
+            for(var i = 1; i < _numbers.Length; i++)
+            {
+                if (_numbers[i] > _numbers[i-1])
+                {
+                    counter++;
+                }
+            }
+            return $"The depth increases {counter} times.";
+        }
+        private string Task2()
+        {
+            var counter = 0;
+            for (var i = 3; i < _numbers.Length; i++)
+            {
+                var previousSet = _numbers.Skip(i - 3).Take(3);
+                var currentSet = _numbers.Skip(i - 2).Take(3);
+
+                if (previousSet.Sum() < currentSet.Sum())
+                {
+                    counter++;
+                }
+            }
+            return $"The depth per set of 3 increases {counter} times";
+        }
+    }
+}
